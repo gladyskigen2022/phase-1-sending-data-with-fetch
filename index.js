c5b2ad494d9a59bd10f3f61aa31331bb02006b38
GladysKigen <gladys.kigen@student.moringa.com>
2022-12-15 23:06:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name,email){
+    const user ={
+        name: name,
+        email: email
+    }
+return fetch("http://localhost:3000/users", {
+   method: "POST",
+   headers: {
+    "Content-Type":"application/json",
+    "Accept": "application/json"
+   },
+   body:JSON.stringify(user)
+
+})
+.then(res => res.json())
+.then(data => {
+    const info =document.createElement('p')
+    info.textContent = data.id
+    document.body.append(info)
+    
+})
+.catch(err=> document.querySelector('body').append('Unauthorized Access'))
+}
